750a19defe3a55572949fdd7ccd1d4f31987aa7b
tancsi <laci19980911@gmail.com>
2020-09-29 15:57:29 +0200

    Update Login.js

--- loginIonic/src/Login.js
+++ loginIonic/src/Login.js
@@ -11,7 +11,132 @@ import {
   IonButtons
 } from '@ionic/react';
 
-const Login = () => (
+const Login = () => {
+
+
+
+  App.tsx
+  fuggveny{
+  const [user, setUser] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [hasAccount, setHasAccount] = useState(false);
+    const [date, setDate] = useState(new Date());
+
+    const clearInputs = () => {
+      setEmail('');
+      setPassword('');
+    }
+
+    const clearErrors = () => {
+      setEmailError('');
+      setPasswordError('');
+    }
+
+    const handleLogin = () => {
+      var db = fire.firestore();
+      var successful=true;
+        clearErrors();
+        fire
+          .auth()
+          .signInWithEmailAndPassword(email,password)
+          .catch((err) => {
+            switch(err.code){
+              case "auth/invalid-email":
+              case "auth/user-disabled":
+              case "auth/user-not-found":
+                setEmailError(err.message);
+                break;
+              case "auth/wrong-password":
+                setPasswordError(err.message);
+                break;
+            }
+          });
+          if (successful)
+          {
+            db.collection("Users").get().then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+
+    			if(email == doc.get("Username")){
+
+    					if(doc.get("Admin") === "true"){
+    						console.log("Admin");
+    					}else{
+    						console.log("User");
+    					}
+
+    			}
+
+    		});
+    	});
+          }
+    };
+
+    const handleSignup = () => {
+
+      var db = fire.firestore();
+      var successful=true;
+      clearErrors();
+      fire
+        .auth()
+        .createUserWithEmailAndPassword(email,password)
+        .catch((err) => {
+          successful=false;
+          switch(err.code){
+            case "auth/email-already-in-use":
+            case "auth/invalid-email":
+              setEmailError(err.message);
+              break;
+            case "auth/weak-password":
+              setPasswordError(err.message);
+              break;
+          }
+        }).finally(()=>{
+          if (successful)
+        {
+          db.collection("Users").add({
+            Admin : "false",
+            Username: email
+          })
+        }});
+
+
+    };
+
+    const handleLogout = () => {
+      fire.auth().signOut();
+    };
+
+    const authListener = () => {
+      fire.auth().onAuthStateChanged((user) => {
+        if (user){
+          clearInputs();
+          setUser(user);
+        }
+        else {
+          setUser("");
+        }
+      });
+    };
+
+    useEffect(() => {
+      authListener();
+    }, []);
+
+    const onDateChange = date => {
+      setDate(date);
+    };
+  };
+
+
+
+
+
+
+  return
+  (
   <>
     <IonHeader>
       <IonToolbar>
@@ -26,6 +151,6 @@ const Login = () => (
 
     </IonContent>
   </>
-);
 
+);}
 export default Login;
